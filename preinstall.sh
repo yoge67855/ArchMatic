f2503f4bb6db09e87a7aa174083320401d3c3b46
yoge67855 <57288159+yoge67855@users.noreply.github.com>
2020-12-27 13:54:33 +0100

    Update preinstall.sh

--- preinstall.sh
+++ preinstall.sh
@@ -7,16 +7,6 @@
 #  Arch Linux Post Install Setup and Config
 #-------------------------------------------------------------------------
 
-echo "-------------------------------------------------"
-echo "Setting up mirrors for optimal download - PL Only"
-echo "-------------------------------------------------"
-timedatectl set-ntp true
-pacman -S --noconfirm pacman-contrib
-mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-curl -s "https://www.archlinux.org/mirrorlist/?country=PL&protocol=https&use_mirror_status=on" | sed -e 's/^#Server/Server/' -e '/^#/d' | rankmirrors -n 5 - > /etc/pacman.d/mirrorlist
-
-
-
 echo -e "\nInstalling prereqs...\n$HR"
 pacman -S --noconfirm gptfdisk btrfs-progs
 
